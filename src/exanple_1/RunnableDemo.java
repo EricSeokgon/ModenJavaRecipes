a5ffc7bfd08228f2d918a26efca98631549b2732
sklee <hadesleeq@gmail.com>
2017-09-13 11:18:42 +0900

    Initial commit

---
+++ src/exanple_1/RunnableDemo.java
@@ -0,0 +1,21 @@
+package exanple_1;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: RunnableDemo
+ * Date: 2017-09-13
+ * Time: 오전 11:15
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class RunnableDemo {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("inside runnable using an anonymous inner class");
+            }
+        }).start();
+    }
+}
