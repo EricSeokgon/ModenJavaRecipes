4ee3507f3f04e14fc251afe3d5184b2c90f2a9de
sklee <hadesleeq@gmail.com>
2017-09-14 15:44:27 +0900

    shows the same example using a lambda expression.

--- src/exanple_1/RunnableDemo.java
+++ src/exanple_1/RunnableDemo.java
@@ -11,11 +11,13 @@ package exanple_1;
  */
 public class RunnableDemo {
     public static void main(String[] args) {
-        new Thread(new Runnable() {
+        /*new Thread(new Runnable() {
             @Override
             public void run() {
                 System.out.println("inside runnable using an anonymous inner class");
             }
-        }).start();
+        }).start();*/
+
+        new Thread(() -> System.out.println("inside runnable using an anonymous inner class")).start();
     }
 }
