f56d864d7de5b3dd3a709bdfd4463bd77c7ab3fa
sklee <hadesleeq@gmail.com>
2017-09-15 09:34:24 +0900

    Using a method reference to access println

---
+++ src/example_1/Example1_8.java
@@ -0,0 +1,25 @@
+package example_1;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Example1_8
+ * Date: 2017-09-15
+ * Time: 오전 9:30
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example1_8 {
+    public static void main(String[] args) {
+        //Using a lambda expression
+        Stream.of(3, 1, 4, 1, 5, 9).forEach(x -> System.out.println(x));
+        //Using a method reference
+        Stream.of(3, 1, 4, 1, 5, 9).forEach(System.out::println);
+        //Assigning the method reference to a functional interface
+        Consumer<Integer> printer = System.out::println;
+        Stream.of(3, 1, 4, 1, 5, 9).forEach(printer);
+    }
+}
