4bf4b3c603a7a384298bcd47e684ef9124eb4d43
sklee <hadesleeq@gmail.com>
2017-09-15 09:36:46 +0900

    Using a method reference to a static method

---
+++ src/example_1/Example1_9.java
@@ -0,0 +1,20 @@
+package example_1;
+
+import java.util.stream.Stream;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Example1_9
+ * Date: 2017-09-15
+ * Time: 오전 9:34
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example1_9 {
+    public static void main(String[] args) {
+        Stream.generate(Math::random) //Static method
+                .limit(10)
+                .forEach(System.out::println); //Instance method
+    }
+}
