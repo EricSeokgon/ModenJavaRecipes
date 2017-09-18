7d329d0d1b6f457f71dbca01c33aef1083ad117b
sklee <hadesleeq@gmail.com>
2017-09-18 10:40:19 +0900

    A Person class

---
+++ src/example_1/Person.java
@@ -0,0 +1,21 @@
+package example_1;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Person
+ * Date: 2017-09-18
+ * Time: 오전 10:39
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
