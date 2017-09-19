4f033548e58f658acd5b6d1b08ef14b993180d2b
sklee <hadesleeq@gmail.com>
2017-09-19 10:16:35 +0900

    A Palindrome Checker interface

---
+++ src/example_1/PalindromeChecker.java
@@ -0,0 +1,15 @@
+package example_1;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: PalindromeChecker
+ * Date: 2017-09-19
+ * Time: 오전 10:03
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@FunctionalInterface
+public interface PalindromeChecker {
+    boolean isPalidrome(String s);
+}
