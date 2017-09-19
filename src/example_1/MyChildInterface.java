27fcdd4eedc532b25b3711b773186638f2935c91
sklee <hadesleeq@gmail.com>
2017-09-19 10:53:13 +0900

    Extending a functional interface — no longer functional

---
+++ src/example_1/MyChildInterface.java
@@ -0,0 +1,14 @@
+package example_1;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: MyChildInterface
+ * Date: 2017-09-19
+ * Time: 오전 10:52
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MyChildInterface extends MyInterface {
+    int anotherMehtod();
+}
