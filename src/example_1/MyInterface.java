f081df927ba5ae447a11dd84318d91492e1c2e84
sklee <hadesleeq@gmail.com>
2017-09-19 10:43:10 +0900

    MyInterface is a functional interface with static and default methods

---
+++ src/example_1/MyInterface.java
@@ -0,0 +1,21 @@
+package example_1;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: MyInterface
+ * Date: 2017-09-19
+ * Time: 오전 10:16
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@FunctionalInterface
+public interface MyInterface {
+    int myMethod();
+    default String sayHello() {
+        return "Hello, world!";
+    }
+    static void myStaticMethod() {
+        System.out.println("I'm a static method in an interface.");
+    }
+}
