505bc68d5fe4d5caedd7e48e5638ed2560ef188e
sklee <hadesleeq@gmail.com>
2017-09-20 09:34:10 +0900

    Using Math.random() as a Supplier

---
+++ src/example_2/Example2_4.java
@@ -0,0 +1,31 @@
+package example_2;
+
+import java.util.function.DoubleSupplier;
+import java.util.logging.Logger;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Example2_4
+ * Date: 2017-09-20
+ * Time: 오전 9:27
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example2_4 {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("...");
+
+        DoubleSupplier randomSupplier = new DoubleSupplier() {
+            @Override
+            public double getAsDouble() {
+                return Math.random();
+            }
+        };
+
+        //randomSupplier = () -> Math.random();
+        //randomSupplier = Math::random;
+
+        logger.info(String.valueOf(randomSupplier));
+    }
+}
