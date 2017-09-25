4eb05fd5fb4656081485a3f17c4bcac24436584e
sklee <hadesleeq@gmail.com>
2017-09-25 13:12:49 +0900

    Creating a stream using Stream.iterate

---
+++ src/Chapter3/Example3_4.java
@@ -0,0 +1,24 @@
+package Chapter3;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Example3_4
+ * Date: 2017-09-25
+ * Time: 오후 1:11
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example3_4 {
+    public static void main(String[] args) {
+        List<BigDecimal> nums = Stream.iterate(BigDecimal.ONE, n -> n.add(BigDecimal.ONE))
+                .limit(10)
+                .collect(Collectors.toList());
+        System.out.println(nums);
+    }
+}
