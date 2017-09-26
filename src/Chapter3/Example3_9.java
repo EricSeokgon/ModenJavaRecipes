c52bdd6b1ac2d57785e12758336edecc9e755c3e
sklee <hadesleeq@gmail.com>
2017-09-26 11:15:24 +0900

    Converting a stream of strings to a list

---
+++ src/Chapter3/Example3_9.java
@@ -0,0 +1,22 @@
+package Chapter3;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Example3_9
+ * Date: 2017-09-26
+ * Time: 오전 11:13
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example3_9 {
+    public static void main(String[] args) {
+        List<Integer> ints =  IntStream.of(3, 1, 4, 1, 5, 9)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
