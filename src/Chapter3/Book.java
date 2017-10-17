1e2960c3b676cfe1bb0b28cfcebb910334c55f8a
sklee <hadesleeq@gmail.com>
2017-10-17 09:11:59 +0900

    A simple Book class

---
+++ src/Chapter3/Book.java
@@ -0,0 +1,15 @@
+package Chapter3;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Book
+ * Date: 2017-10-17
+ * Time: 오전 9:11
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+    private Integer id;
+    private String title;
+}
