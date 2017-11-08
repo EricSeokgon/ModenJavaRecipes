08e27d633f8491850aa816d0995dd04be46c37f3
sklee <hadesleeq@gmail.com>
2017-11-08 10:14:52 +0900

    A class for golfers

---
+++ src/Chapter4/Golfer.java
@@ -0,0 +1,16 @@
+package Chapter4;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Golfer
+ * Date: 2017-11-08
+ * Time: 오전 10:13
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Golfer {
+    private String first;
+    private String last;
+    private int score;
+}
