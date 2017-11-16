cd0936947de395f948ec1079656191d97900069f
sklee <hadesleeq@gmail.com>
2017-11-16 09:36:21 +0900

    A simple POJO representing a book

---
+++ src/Chapter4/Book.java
@@ -0,0 +1,16 @@
+package Chapter4;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Example4_12
+ * Date: 2017-11-16
+ * Time: 오전 9:33
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+    private int id;
+    private String name;
+    private double price;
+}
