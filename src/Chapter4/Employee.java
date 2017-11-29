b25d10fa813cae27b48ebda038fd8d9847a8e2ff
sklee <hadesleeq@gmail.com>
2017-11-29 09:08:23 +0900

    Employee POJO

---
+++ src/Chapter4/Employee.java
@@ -0,0 +1,16 @@
+package Chapter4;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Employee
+ * Date: 2017-11-29
+ * Time: 오전 9:07
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+    private String name;
+    private Integer salary;
+    private String department;
+}
