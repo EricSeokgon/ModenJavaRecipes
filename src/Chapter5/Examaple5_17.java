94484dad5b6f1a382cecfa8a4c0b42522e6c456b
sklee <hadesleeq@gmail.com>
2018-01-15 08:46:13 +0900

    Iterating over a linear collection

--- src/Chapter5/Examaple5_17.java
+++ src/Chapter5/Examaple5_17.java
@@ -6,14 +6,14 @@ import java.util.function.Consumer;
 
 /**
  * Project: ModenJavaRecipes
- * FileName: Example5_17
- * Date: 2018-01-12
- * Time: 오전 8:46
+ * FileName: Examaple5_17
+ * Date: 2018-01-15
+ * Time: 오전 8:45
  * Author: user
  * Note:
  * To change this template use File | Settings | File Templates.
  */
-public class Example5_17 {
+public class Examaple5_17 {
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(3, 1, 4, 1, 5, 9);
         integers.forEach(new Consumer<Integer>() {
@@ -22,5 +22,10 @@ public class Example5_17 {
                 System.out.println(integer);
             }
         });
+        integers.forEach((Integer n) -> {
+            System.out.println(n);
+        });
+        integers.forEach(n -> System.out.println(n));
+        integers.forEach(System.out::println);
     }
 }
