5346c17844fb04ed3fa48ad835fa74d98ea4e418
sklee <hadesleeq@gmail.com>
2018-03-12 08:42:45 +0900

    Using Files.list(path)

---
+++ src/Chapter7/Example7_6.java
@@ -0,0 +1,26 @@
+package Chapter7;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * Project: ModenJavaRecipes
+ * FileName: Example7_6
+ * Date: 2018-03-12
+ * Time: 오전 8:41
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example7_6 {
+    public static void main(String[] args) {
+        try (Stream<Path> list = Files.list(Paths.get("C:\\Users\\user\\IdeaProjects\\ModenJavaRecipes\\src\\Chapter7\\Example7_6.java"))) {
+            list.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
