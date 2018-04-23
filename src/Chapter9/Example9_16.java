a7e2a2d998e598c12dd36a080fea54d1d5db67d5
sklee <hadesleeq@gmail.com>
2018-04-23 09:32:00 +0900

    Retrieving a value from a Future

---
+++ src/Chapter9/Example9_16.java
@@ -0,0 +1,18 @@
+package Chapter9;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class Example9_16 {
+    public void getIfNotCancelled(Future<String> future) {
+        try {
+            if (!future.isCancelled()) {
+                System.out.println(future.get());
+            } else {
+                System.out.println("Cancelled");
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
