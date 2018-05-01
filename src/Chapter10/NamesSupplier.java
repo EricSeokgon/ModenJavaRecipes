991b20842d78197b03ee795c7bb73bbbafb27434
sklee <hadesleeq@gmail.com>
2018-05-01 13:06:23 +0900

    Supplying a stream of names

---
+++ src/Chapter10/NamesSupplier.java
@@ -0,0 +1,21 @@
+package Chapter10;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class NamesSupplier implements Supplier<Stream<String>> {
+    private Path namesPath = Paths.get("server/src/main/resources/names.txt");
+    @Override
+    public Stream<String> get() {
+        try {
+            return Files.lines(namesPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
