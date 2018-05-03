921ab9482fd42c8bc8eab4e68db8ec2ea6a1f3c0
sklee <hadesleeq@gmail.com>
2018-05-03 14:47:24 +0900

    Static factory methods for creating immutable lists

---
+++ src/Chapter10/Example10_10.java
@@ -0,0 +1,16 @@
+package Chapter10;
+
+public class Example10_10 {
+    static <E> List<E> of()
+    static <E> List<E> of(E e1)
+    static <E> List<E> of(E e1, E e2)
+    static <E> List<E> of(E e1, E e2, E e3)
+    static <E> List<E> of(E e1, E e2, E e3, E e4)
+    static <E> List<E> of(E e1, E e2, E e3, E e4, E e5)
+    static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6)
+    static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7)
+    static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8)
+    static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9)
+    static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10)
+    static <E> List<E> of(E... elements)
+}
